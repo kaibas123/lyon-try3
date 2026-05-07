1c02a3c3957714f07e2462067f161708d8aac75d
kaibas123 <sdhlife0419@gmail.com>
2026-05-07 15:35:50 +0900

    make-C11

---
+++ C9/back-end/cors.php
@@ -0,0 +1,8 @@
+<?php
+
+/** EDIT AREA **/
+if ($_SERVER['REQUEST_METHOD'] === "OPTION") return false;
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: *");
